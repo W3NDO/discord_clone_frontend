a6582aa5feae4bb4abb1b083124b523d9f2202a5
steekam <waynewanyee@gmail.com>
2021-04-17 22:24:49 +0300

    wip: scaffolded main very basic UI layout.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <ServerSideBar />
+      <ChannelsColumn />
+      <ChatColumn />
+    </div>
+  );
+}
+
+
+function ServerSideBar() {
+  return (
+    <div className="server">
+
+    </div>
+  );
+}
+
+function ChannelsColumn() {
+  return (
+    <div className="channels">
+
+    </div>
+  );
+}
+
+function ChatColumn() {
+  return (
+    <div className="chat">
+      <header> </header>
+      <div className="grid">
+        <div className="texts">
+        </div>
+
+        <div className="users">
+
+        </div>
+      </div>
     </div>
   );
 }
